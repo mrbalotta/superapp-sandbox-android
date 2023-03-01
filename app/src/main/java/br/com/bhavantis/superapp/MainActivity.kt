be93498e7a02b64eb2a31af1ba9e1c4731f4d4b8
Alessandro Balotta Oliveira <abalotta@emeal.nttdata.com>
2023-03-01 10:25:19 -0300

    first commit

---
+++ app/src/main/java/br/com/bhavantis/superapp/MainActivity.kt
@@ -0,0 +1,43 @@
+package br.com.bhavantis.superapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.com.bhavantis.superapp.ui.theme.SuperappDemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SuperappDemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    SuperappDemoTheme {
+        Greeting("Android")
+    }
+}
